2971855c314a6745098ed262a92e5313543a9384
Fabian0104 <fabiancruz0104@gmail.com>
2023-09-30 20:01:20 -0600

    clase 9-30-23

--- proyectpoo/src/com/poo/App.java
+++ proyectpoo/src/com/poo/App.java
@@ -1,17 +1,39 @@
 package com.poo;
 
-import com.poo.model.Persona;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.poo.model.Estudiante;
+import com.poo.model.Materia;
+import com.poo.service.IEstudianteService;
+import com.poo.service.impl.EstudianteServiceImpl;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-        Persona persona = new Persona(
-            1, 
-            "Pedro",
-            "M", 
-            "2006-01-28",
-            true
-             );
-        System.out.println(persona);
-    }   
+
+        IEstudianteService iEstudianteService = new EstudianteServiceImpl();
+             
+        Estudiante estudiante = new Estudiante(
+            "Dante", "M", "2006-08-26",
+            "CEDUK",
+            "Ingenieria en sistemas",
+            4,
+            1001);
+
+        List<Materia> materias = new ArrayList<Materia>();
+
+        Materia matematicas = new Materia("Matematicas", 9.0);
+        Materia espanol = new Materia("Español", 8.0);
+        Materia ingles = new Materia("Inglés", 7.7);
+
+        materias.add(matematicas);
+        materias.add(espanol);
+        materias.add(ingles);
+
+        estudiante.setPromedio(iEstudianteService.promedioFinal(materias));
+
+        System.out.println(estudiante);
+    }  
     
 }
